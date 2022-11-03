5c471d90482f135714e9c4e297cef8d30f4ca80a
a.cheremushkin <a.ch3re359@yandex.ru>
2022-11-03 22:03:07 +0400

    Change test engine-variable.(x6)

--- jun_jobs_bot/models.py
+++ jun_jobs_bot/models.py
@@ -8,9 +8,10 @@ DB_PASSWORD = os.environ['DB_PASSWORD']
 HOST = os.environ['HOST']
 PORT = os.environ['PORT']
 DATABASE = os.environ['DATABASE']
-CONNECT = 'postgresql+psycopg2://%s:%s@%s:%s/%s' % (DB_USERNAME, DB_PASSWORD,
-                                                    HOST, PORT,
-                                                    DATABASE)
+
+CONNECT = 'postgresql://%s:%s@%s:%s/%s' % (DB_USERNAME, DB_PASSWORD,
+                                           HOST, PORT,
+                                           DATABASE)
 
 engine = create_engine(CONNECT)
 
