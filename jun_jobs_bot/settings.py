590499246db443413e96d04788a17cab4e3d0f64
a.cheremushkin <a.ch3re359@yandex.ru>
2022-11-08 18:55:37 +0400

    Try run CI

--- jun_jobs_bot/settings.py
+++ jun_jobs_bot/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-TG_TOKEN = os.environ['TG_TOKEN']
+TG_TOKEN = os.getenv('TG_TOKEN')
 BASE_DIR = Path(__file__).resolve().parent.parent
-ADMIN_ID = os.environ['ADMIN_ID']
-DB_CONNECT = os.environ['DB_CONNECT']
+ADMIN_ID = os.getenv('ADMIN_ID')
+DB_CONNECT = os.getenv('DB_CONNECT')
