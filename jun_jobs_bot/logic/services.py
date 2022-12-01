a4dedc15c1a42e1a2baaf34cd427c21f506484b8
a.cheremushkin <a.ch3re359@yandex.ru>
2022-12-01 19:59:31 +0400

    Move services functions from statistics.py to services.py, rename messages.py

---
+++ jun_jobs_bot/logic/services.py
@@ -0,0 +1,39 @@
+from loguru import logger
+from .statistics import Stats
+from .exceptions import NotCorrectData
+from .. import text
+from ..dataclasses import LANGUAGES_ID, COMPARE_TYPE, AVAILABLE_LANGUAGES
+
+
+def process_request_data(data: dict[str, str]) -> tuple[str, str]:
+    language, compare_type = data['language'], data['compare_type']
+    processed_compare_type = compare_type.lower().replace(' ', '')
+    return language, processed_compare_type
+
+
+def get_statistics(language: str, compare_type: str) -> str:
+    language_id = LANGUAGES_ID[language.lower()]
+    stat = Stats(language, language_id)
+
+    if compare_type == COMPARE_TYPE.right_now:
+        today_data = stat.get_today_stat()
+        return stat.return_today_stats(today_data)
+
+    now, past_time = stat.get_stats_by_comparison_type(compare_type)
+    compute_state = stat.compute_stats(now.vacancies, past_time.vacancies)
+    return stat.return_stat_by_comp_type(compute_state)
+
+
+def validate_data(data: dict[str, str]) -> None:
+    language, compare_type = data['language'], data['compare_type']
+
+    if language.lower() not in AVAILABLE_LANGUAGES:
+        logger.error(f'Not correct data. Language: {language.lower()}')
+        raise NotCorrectData(text.MessageReply.NOT_CORRECT_LANG)
+
+    if compare_type.lower().replace(' ', '') not in COMPARE_TYPE:
+
+        logger.error(f'Not correct data. Compare type: '
+                     f'{compare_type.lower()}')
+
+        raise NotCorrectData(text.MessageReply.NOT_CORRECT_COMPARE_TYPE)
--- jun_jobs_bot/logic/statistics.py
+++ jun_jobs_bot/logic/statistics.py
@@ -1,27 +1,8 @@
-from jun_jobs_bot.logic.exceptions import NotCorrectData
-from jun_jobs_bot.dataclasses import COMPARE_TYPE, LANGUAGES_ID, \
-    AVAILABLE_LANGUAGES, CONSTANTS
-from jun_jobs_bot import messages as msg
-from jun_jobs_bot.models import Statistics
 from loguru import logger
+from jun_jobs_bot.dataclasses import CONSTANTS
+from jun_jobs_bot.models import Statistics
+from jun_jobs_bot.text import MessageReply
 from jun_jobs_bot.logic.db_work import DatabaseWorker
-
-
-def process_request_data(data: dict[str, str]) -> tuple[str, str]:
-    language, compare_type = data['language'], data['compare_type']
-    processed_compare_type = compare_type.lower().replace(' ', '')
-    return language, processed_compare_type
-
-
-def get_statistics(language: str, compare_type: str) -> str:
-    language_id = LANGUAGES_ID[language.lower()]
-    stat = Stats(language, language_id)
-    if compare_type == COMPARE_TYPE.right_now:
-        today_data = stat.get_today_stat()
-        return stat.return_today_stats(today_data)
-    now, past_time = stat.get_stats_by_comparison_type(compare_type)
-    compute_state = stat.compute_stats(now.vacancies, past_time.vacancies)
-    return stat.return_stat_by_comp_type(compute_state)
 
 
 class Stats:
@@ -35,11 +16,10 @@ class Stats:
     def get_today_stat(self) -> str | tuple[int, int]:
         vacancies = self.db_worker.get_today_stat(self.language_id)
 
-        logger.info(f'Data: {self.language}, {vacancies.vacancies}, '
-                    f'{vacancies.date}')
-
         if vacancies is None:
-            return msg.MessageReply.HAVE_NO_DATE
+            logger.info(f'vacancies is {str(vacancies)}')
+            return MessageReply.HAVE_NO_DATE
+        logger.info(f'{vacancies}')
         return vacancies.vacancies, vacancies.no_experience
 
     def get_stats_by_comparison_type(
@@ -55,37 +35,26 @@ class Stats:
                      CONSTANTS['Coefficient'] - CONSTANTS['Coefficient'])
 
     def return_today_stats(self, vacancies: tuple[int, int]) -> str:
-        """Returns the final line with today's statistics"""
+        """Returns the final string with today's statistics"""
         if isinstance(vacancies, str):
             return vacancies
-        return msg.TODAY_STAT.substitute(
+        return MessageReply.TODAY_STAT.substitute(
             language=self.language,
             all_vacancies=vacancies[0],
             no_exp_vacancies=vacancies[1],
         )
 
     def return_stat_by_comp_type(self, vacancies: int) -> str:
-        """Returns the final line with statistics """
+        """return the final string depending on the type of comparison"""
         if vacancies < 0:
-            return msg.VACS_DECREASED.substitute(
+            return MessageReply.VACS_DECREASED.substitute(
                 language=self.language,
                 result=vacancies,
             )
         elif vacancies > 0:
-            return msg.VACS_INCREASED.substitute(
+            return MessageReply.VACS_INCREASED.substitute(
                 language=self.language,
                 result=vacancies,
             )
         else:
-            return msg.VACS_NO_CHANGE
-
-
-def validate_data(data: dict[str, str]) -> None:
-    language, compare_type = data['language'], data['compare_type']
-    if language.lower() not in AVAILABLE_LANGUAGES:
-        logger.error(f'Not correct data. Language: {language.lower()}')
-        raise NotCorrectData(msg.NOT_CORRECT_LANG)
-    if compare_type.lower().replace(' ', '') not in COMPARE_TYPE:
-        logger.error(f'Not correct data. Compare type: '
-                     f'{compare_type.lower()}')
-        raise NotCorrectData(msg.NOT_CORRECT_COMPARE_TYPE)
+            return MessageReply.VACS_NO_CHANGE
